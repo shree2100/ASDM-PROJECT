909cec80e883757653196b8a9bba17698d7263fd
shree2100 <129053050+shree2100@users.noreply.github.com>
2023-03-31 00:03:41 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,21 @@
+package day2;
+
+public class operators {
+	public static void main(String[]args)
+	
+	}
+
+	int x=10,
+	    y=20;
+	    
+	int result = (x>y)? (x+y):(x-y);
+    
+	System.out.println(result);
+	private void sysout() {
+		// TODO Auto-generated method stub
+
+	}
+
+	}
+	
+}
